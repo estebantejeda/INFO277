1349f5e9787d1e352f0d0797558b31c3b2b98249
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-09-09 03:19:55 -0300

    Creado controllers para cliente y producto

---
+++ controllers/customer.ts
@@ -0,0 +1,8 @@
+import {Request, Response} from 'express';
+import Customer from "../models/customer";
+
+export const setCustomer = (req: Request, res: Response) => {
+    Customer.create({...req.body})
+        .then(result => res.json({result, ok: true}))
+        .catch(err => res.json(err));
+}
---
+++ controllers/product.ts
@@ -0,0 +1,8 @@
+import {Request, Response} from 'express';
+import Product from "../models/product";
+
+export const setProduct = (req: Request, res: Response) => {
+    Product.create({...req.body})
+        .then(result => res.json({result, ok: true}))
+        .catch(err => res.json(err));
+}
