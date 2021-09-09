47a336e9c39b258e6159a01943bbfbf801cc1645
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-09-09 16:09:35 -0300

    Añadidos más controladores para productos

--- controllers/product.ts
+++ controllers/product.ts
@@ -12,3 +12,28 @@ export const setProduct = (req: Request, res: Response) => {
         .then(result => res.json({result, ok: true}))
         .catch(err => res.json({err, ok: false}));
 }
+
+export const getProduct = (req: Request, res: Response) => {
+    Product.findByPk(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.json(err));
+}
+
+export const getAllProduct = (_req: Request, res: Response) => {
+    Product.findAll()
+        .then(post => res.json(post))
+        .catch(err => res.json(err));
+}
+
+export const putProduct = (req: Request, res: Response) => {
+    Product.update({
+        description: req.body.description,
+        cost: req.body.cost,
+        stock: req.body.stock,
+        price: req.body.price
+    },{
+        where: {id: req.params.id}
+    })
+        .then(post => res.json(post))
+        .catch(err => res.json(err));
+}
--- routes/product.ts
+++ routes/product.ts
@@ -1,8 +1,11 @@
 import {Router} from 'express';
-import {setProduct} from '../controllers/product';
+import * as product from '../controllers/product';
 
 const router = Router()
 
-router.post('/product', setProduct);
+router.post('/product', product.setProduct);
+router.get('/product', product.getAllProduct);
+router.get('/product/:id', product.getProduct);
+router.put('/product/:id', product.putProduct);
 
 export default router;
