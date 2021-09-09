6534c6fcfc9d9aa51587cf3be9fb94c9e6fb1dff
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-09-09 03:20:51 -0300

    Creado un index para routes

--- app.ts
+++ app.ts
@@ -1,19 +1,13 @@
 import express from 'express';
-import database from './config/database';
 import Config from './config/config';
-import Product from "./models/product";
+import database from './config/database';
+import router from './routes/index';
 
 const config = new Config();
-
 const app = express();
+
 app.use(express.json());
-
-
-app.get("/", (req, res) => {
-    Product.create({...req.body})
-        .then((msj) => res.json(msj))
-        .catch(err => res.json(err));
-});
+app.use(router);
 
 database.sync({force: true}).then(() => {
     app.listen(config.PORT, () => console.log("Conexión establecida"));
---
+++ routes/index.ts
@@ -0,0 +1,11 @@
+import {Router} from 'express';
+
+import product from './product';
+import customer from "./customer";
+
+const router = Router()
+
+router.use(product);
+router.use(customer);
+
+export default router;
