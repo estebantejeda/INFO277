87606008970a10619b7b7a2ae9d37361309622eb
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-09-12 02:15:45 -0300

    Cambios de variables predeterminadas en config

--- config/config.ts
+++ config/config.ts
@@ -3,8 +3,8 @@ import path from 'path';
 
 dotenv.config({path: path.resolve(__dirname, '../../.env')});
 
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASS = process.env.DB_PASS || "toor";
-export const DB_NAME = process.env.DB_NAME || "test";
+export const DB_USER = process.env.DB_USER || "admin";
+export const DB_PASS = process.env.DB_PASS || "admin";
+export const DB_NAME = process.env.DB_NAME || "erp";
 export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
 export const PORT = parseInt(process.env.PORT as string)|| 3000;
