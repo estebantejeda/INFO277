7e5f0f03267d6e576db3d3845152f92bba8dcb59
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-10-23 00:41:27 -0300

    Cambiadas las variables del modelo saleLedger. Se realización las asociaciones correspondientes con las demás tablas

--- models/associations.ts
+++ models/associations.ts
@@ -1,15 +1,28 @@
 import Product from "./product";
 import PurchaseLedger from "./purchaseLedger";
 import Provider from "./provider";
+import SaleLedger from "./saleLedger";
 
 const init = () => {
     Product.hasOne(PurchaseLedger, {
         foreignKey: {allowNull: false}
     });
+    PurchaseLedger.belongsTo(Product);
 
     Provider.hasOne(PurchaseLedger,{
         foreignKey: {allowNull: false}
     });
+    PurchaseLedger.belongsTo(Provider);
+
+    Product.hasOne(SaleLedger, {
+        foreignKey: {allowNull: false}
+    });
+    PurchaseLedger.belongsTo(Product);
+
+    Provider.hasOne(SaleLedger, {
+        foreignKey: {allowNull: false}
+    });
+    SaleLedger.belongsTo(Provider);
 }
 
 export default init;
--- models/saleLedger.ts
+++ models/saleLedger.ts
@@ -2,34 +2,31 @@ import {DataTypes} from "sequelize";
 import database from "../config/database";
 
 const SaleLedger = database.define('sale_ledger', {
-    month: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate:{
-            min: 1,
-            max: 12
-        }
-    },
-    year: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {len: [4,4]}
-    },
     docNumber: {
         type: DataTypes.INTEGER,
         allowNull: false,
         field: 'doc_number'
     },
-    netValue: {
-        type: DataTypes.DOUBLE,
-        allowNull: false,
-        field: 'net_value'
+    date: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
     },
-    tax: {
-        type: DataTypes.DOUBLE,
+    docType: {
+        type: DataTypes.ENUM,
+        values: ["factura", "boleta"]
     },
+    payType: {
+        type: DataTypes.ENUM,
+        values: ["efectivo", "credito", "debito"]
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        validate: {min: 1}
+    },
+    price: DataTypes.INTEGER,
     total: {
-        type: DataTypes.DOUBLE,
+        type: DataTypes.INTEGER,
         allowNull: false
     }
 });
