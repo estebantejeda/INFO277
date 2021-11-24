21366992388d327839fdd3641a53d94d30e8c1ef
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-11-24 04:25:26 -0300

    Comentarios leves

--- controllers/purchase.ts
+++ controllers/purchase.ts
@@ -49,6 +49,7 @@ export const getAllPurchase = async (_req: Request, res: Response) => {
 }
 
 export const getSumPurchase = async (_req: Request, res: Response) => {
+    //SELECT accountId, name, SUM(net) FROM purchases JOIN accounts ON purchases.accountId=accounts.id GROUP BY accountId;
     const purchases = await Purchase.findAll({
         attributes: [
             'accountId',
